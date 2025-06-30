025ef910fe720b6176f873fd150dcfe9dfcff75b
Ashutosh Bhargava <ashutumho@gmail.com>
2025-06-30 23:51:24 +0530

    Updated the UI and wave calcualtion logic

--- Assets/Scripts/NeuroskyGUIManager.cs
+++ Assets/Scripts/NeuroskyGUIManager.cs
@@ -24,8 +24,15 @@ namespace MindWave
         private float lowGammaValue = -1;
         private float highGammaValue = -1;
 
-        private float minValue = 1000f;
-        private float maxValue = 100000f;
+        private float minValue = 0f;
+        private float deltaMaxValue = 100f;
+        private float thetaMaxValue = 100f;
+        private float lowAlphaMaxValue = 100f;
+        private float highAlphaMaxValue = 100f;
+        private float lowBetaMaxValue = 100f;
+        private float highBetaMaxValue = 100f;
+        private float lowGammaMaxValue = 100f;
+        private float highGammaMaxValue = 100f;
 
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
@@ -63,47 +70,55 @@ namespace MindWave
 
         private void OnDeltaReceived(float value)
         {
-            deltaValue = Normalize(value,minValue,maxValue);
+            if(value> deltaMaxValue) deltaMaxValue = value;
+            deltaValue = Normalize(value,minValue, deltaMaxValue);
             UIStatusManager.Instance.UpdatePlayerStatus(2, (int)deltaValue);
         }
 
         private void OnThetaReceived(float value)
         {
-            thetaValue = Normalize(value, minValue, maxValue);
+            if (value > thetaMaxValue) thetaMaxValue = value;
+            thetaValue = Normalize(value, minValue, thetaMaxValue);
             UIStatusManager.Instance.UpdatePlayerStatus(3, (int)thetaValue);
         }
 
         private void OnLowAlphaReceived(float value)
         {
-            lowAlphaValue = Normalize(value, minValue, maxValue);
+            if (value > lowAlphaMaxValue) lowAlphaMaxValue = value;
+            lowAlphaValue = Normalize(value, minValue, lowAlphaMaxValue);
             UIStatusManager.Instance.UpdatePlayerStatus(4, (int)lowAlphaValue);
         }
 
         private void OnHighAlphaReceived(float value)
         {
-            highAlphaValue = Normalize(value, minValue, maxValue);
+            if (value > highAlphaMaxValue) highAlphaMaxValue = value;
+            highAlphaValue = Normalize(value, minValue, highAlphaMaxValue);
             UIStatusManager.Instance.UpdatePlayerStatus(5, (int)highAlphaValue);
         }
 
         private void OnLowBetaReceived(float value)
         {
-            lowBetaValue = Normalize(value, minValue, maxValue);
+            if (value > lowBetaMaxValue) lowBetaMaxValue = value;
+            lowBetaValue = Normalize(value, minValue, lowBetaMaxValue);
             UIStatusManager.Instance.UpdatePlayerStatus(6, (int)lowBetaValue);
         }
 
         private void OnHighBetaReceived(float value)
         {
-            highBetaValue = Normalize(value, minValue, maxValue);
+            if (value > highBetaMaxValue) highBetaMaxValue = value;
+            highBetaValue = Normalize(value, minValue, highBetaMaxValue);
             UIStatusManager.Instance.UpdatePlayerStatus(7, (int)highBetaValue);
         }
         private void OnLowGammaReceived(float value)
         {
-            lowGammaValue = Normalize(value, minValue, maxValue);
+            if (value > lowGammaMaxValue) lowGammaMaxValue = value;
+            lowGammaValue = Normalize(value, minValue, lowGammaMaxValue);
             UIStatusManager.Instance.UpdatePlayerStatus(8, (int)lowGammaValue);
         }
         private void OnHighGammaReceived(float value)
         {
-            highGammaValue = Normalize(value, minValue, maxValue);
+            if (value > highGammaMaxValue) highGammaMaxValue = value;
+            highGammaValue = Normalize(value, minValue, highGammaMaxValue);
             UIStatusManager.Instance.UpdatePlayerStatus(9, (int)highGammaValue);
         }
 
